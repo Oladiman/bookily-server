9b6d37bac18a64d601209ffc69b1c8e68cebeb77
Oladiman <astonishingdami9@gmail.com>
2020-05-12 18:15:29 +0100

    changed the message on home route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,11 @@ const passport=require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 module.exports=(app)=>{
-//   app.get('/api/current_user',(req,res)=>{
-//     res.send(req.user)
-// })
+  app.get('/',(req,res)=>{
+    res.send({
+      Message: "Welcome to Bookily-Server"
+    })
+})
 
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile','email'] }));
