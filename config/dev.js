faf31b6d93fb342acdc72ee7ed3b34c923cbf4dd
dee <Astonishingdami9@gmail.com>
2020-05-12 08:28:31 -0700

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    google_client_ID:"590360212814-6m7cd4c0lslsj7r9t456tl7mb1ukr7ol.apps.googleusercontent.com",
-    google_client_SECRET:"dnz0rfWC53MIiPdCqJs4w5oI",
-    connectionString:"mongodb+srv://Oladiman:Damilola9@cluster0-9y4qr.gcp.mongodb.net/bookily-dev?retryWrites=true&w=majority",
-    cookieKey:'randomkeyIcouldThinkOf'
-}
